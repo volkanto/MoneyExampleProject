a3202ac74a90a382e51f637072b5638cbeda1aa1
volkanto <dev@tokmak.org>
2015-04-21 22:44:26 +0300

    intial commit

---
+++ MoneyExampleProject/src/com/tokmak/example/money/Money.java
@@ -0,0 +1,143 @@
+package com.tokmak.example.money;
+
+/**
+ * <b>Project MoneyExampleProject</b><br />
+ * Money.java<br />
+ *
+ * <b>created at</b> Apr 21, 2015 10:14:31 PM
+ * @author Volkan Tokmak
+ * @since 2.00.00.0
+ */
+public final class Money
+{
+	private Double amount 			= null;
+	private CurrencyEnum currency 	= null;
+	
+	/**
+	 * <b>Project _MoneyMain</b><br />
+	 * Money.java<br />
+	 *
+	 * <b>created at</b> Apr 21, 2015 10:14:29 PM
+	 * @author Volkan Tokmak
+	 * @since 2.00.00.0
+	 */
+	public enum CurrencyEnum {
+		USD,
+		EURO;
+	}
+	
+	/**
+	 * Money<br />
+	 *
+	 *
+	 * <b>created at</b> Apr 21, 2015 10:14:27 PM
+	 * @since 2.00.00.0
+	 * @author Volkan Tokmak
+	 */
+	public Money() 
+	{
+		this.amount 	= Double.valueOf(0f);
+		this.currency 	= CurrencyEnum.EURO;
+	}
+	
+	/**
+	 * Money<br />
+	 *
+	 * @param argAmount
+	 * @param argCurrency
+	 *
+	 * <b>created at</b> Apr 21, 2015 10:16:00 PM
+	 * @since 2.00.00.0
+	 * @author Volkan Tokmak
+	 */
+	public Money(Double argAmount, CurrencyEnum argCurrency)
+	{
+		this.amount 	= argAmount;
+		this.currency 	= argCurrency;
+	}
+
+	/**
+	 * Money<br />
+	 *
+	 * @param argFactor
+	 * @return
+	 * 
+	 * <b>created at</b> Apr 21, 2015 10:16:13 PM
+	 * @since 2.00.00.0
+	 * @author Volkan Tokmak
+	 */
+	public Money multiplyBy(int argFactor)
+	{
+		this.amount = this.amount * argFactor;
+		return this; 
+	}
+
+	/**
+	 * getAmount<br />
+	 * @return the amount
+	 *
+	 * <b>created at</b> Apr 21, 2015 10:13:32 PM
+	 * @author Volkan Tokmak
+	 * @since 2.00.00.0
+	 */
+	public Double getAmount()
+	{
+		return this.amount;
+	}
+
+	/**
+	 * setAmount<br />
+	 *
+	 * @param argAmount the amount to set
+	 *
+	 * <b>created at</b> Apr 21, 2015 10:13:32 PM
+	 * @author Volkan Tokmak
+	 * @since 2.00.00.0
+	 */
+	public void setAmount(Double argAmount)
+	{
+		this.amount = argAmount;
+	}
+
+	/**
+	 * getCurrency<br />
+	 * @return the currency
+	 *
+	 * <b>created at</b> Apr 21, 2015 10:13:32 PM
+	 * @author Volkan Tokmak
+	 * @since 2.00.00.0
+	 */
+	public CurrencyEnum getCurrency()
+	{
+		return this.currency;
+	}
+
+	/**
+	 * setCurrency<br />
+	 *
+	 * @param argCurrency the currency to set
+	 *
+	 * <b>created at</b> Apr 21, 2015 10:13:32 PM
+	 * @author Volkan Tokmak
+	 * @since 2.00.00.0
+	 */
+	public void setCurrency(CurrencyEnum argCurrency)
+	{
+		this.currency = argCurrency;
+	}
+	
+	/**
+	 * Money<br />
+	 *
+	 * @return
+	 *
+	 * <b>created at</b> Apr 21, 2015 10:24:55 PM
+	 * @since 2.00.00.0
+	 * @author Volkan Tokmak
+	 */
+	@Override
+	public String toString()
+	{
+		return "Money: " + this.amount + " " + this.currency.name();
+	}
+}
---
+++ MoneyExampleProject/src/com/tokmak/example/money/MoneyMain.java
@@ -0,0 +1,41 @@
+package com.tokmak.example.money;
+
+import com.tokmak.example.money.Money.CurrencyEnum;
+
+/**
+ * <b>Project MoneyExampleProject</b><br />
+ * MoneyMain.java<br />
+ *
+ * <b>created at</b> Apr 21, 2015 10:32:54 PM
+ * @author Volkan Tokmak
+ * @since 2.00.00.0
+ */
+public class MoneyMain
+{
+
+	/**
+	 * MoneyMain<br />
+	 *
+	 * @param args
+	 * 
+	 * <b>created at</b> Apr 21, 2015 10:32:52 PM
+	 * @since 2.00.00.0
+	 * @author Volkan Tokmak
+	 */
+	public static void main(String[] args)
+	{
+		Money euro 	= new Money(67.89, CurrencyEnum.EURO);
+		Money usd	= new Money(98.76, CurrencyEnum.USD);
+		
+		System.out.println(euro);
+		euro = euro.multiplyBy(5);
+		System.out.println(euro);
+		
+		System.out.println("------------");
+		
+		System.out.println(usd);
+		usd = usd.multiplyBy(4);
+		System.out.println(usd);
+	}
+
+}
